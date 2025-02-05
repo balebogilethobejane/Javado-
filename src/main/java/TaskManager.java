13bb33501d34e4a0f4da328ece9616c92ce3d4c8
Tshegofatso Ditshego <149037835+TshegofatsoDitshego@users.noreply.github.com>
2025-02-05 16:03:24 +0200

    Update TaskManager.java
    Still trying to install Intellij

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,18 +1,22 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
     public TaskManager() {
         // Initialize tasks list
+        List<String> tasks = new ArrayList<>();
     }
 
     public void addTask(String task) {
-        throw new UnsupportedOperationException("Implement this method!");
+       // throw new UnsupportedOperationException("Implement this method!");
+        this.tasks.add(task);
     }
 
     public List<String> listTasks() {
-        throw new UnsupportedOperationException("Implement this method!");
+        //throw new UnsupportedOperationException("Implement this method!");
+        return this.tasks;
     }
 
     public void deleteTask(String task){
